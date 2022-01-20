ba0f8a35146806663b5581adf223c69ce1141ba2
Edwin Török <edvin.torok@citrix.com>
2022-01-20 11:32:57 +0000

    tools/ocaml/libs/mmap: review: add GOSPEL annotations
    No functional change.
    
    Signed-off-by: Edwin Török <edvin.torok@citrix.com>

--- tools/ocaml/libs/mmap/xenmmap.mli
+++ tools/ocaml/libs/mmap/xenmmap.mli
@@ -15,14 +15,37 @@
  *)
 
 type mmap_interface
+(*@ model mapped_len: integer *)
+
 type mmap_prot_flag = RDONLY | WRONLY | RDWR
 type mmap_map_flag = SHARED | PRIVATE
 
+(*@ function pagesize: integer *)
+
 external mmap : Unix.file_descr -> mmap_prot_flag -> mmap_map_flag -> int -> int
              -> mmap_interface = "stub_mmap_init"
+(*@ m = mmap fd prot mapflag len offset
+    requires len > 0 && exists k >= 0. k * pagesize = offset
+    ensures m.mapped_len = len
+    raises Failure _ -> true *)
+
 external unmap : mmap_interface -> unit = "stub_mmap_final"
+(*@ unmap m
+    consumes m *)
+
 external read : mmap_interface -> int -> int -> string = "stub_mmap_read"
+(*@ s = read m start len
+    requires 0 <= start && 0 <= len
+    checks start <= len && start + len <= m.mapped_len
+*)
+
 external write : mmap_interface -> string -> int -> int -> unit
                = "stub_mmap_write"
+(*@ write m data start len
+    requires 0 <= start && 0 <= len < String.length data
+    checks start <= len && start + len <= m.mapped_len
+*)
 
 external getpagesize : unit -> int = "stub_mmap_getpagesize"
+(*@ n = getpagesize ()
+    ensures n = pagesize *)
