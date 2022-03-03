c85acaeea9fff36c3cc80141804bbd7873c3cc69
Edwin Török <edvin.torok@citrix.com>
2022-03-03 17:47:38 +0000

    WiP: sizeexpr

---
+++ tools/ocaml/libs/xb/memory_size.ml
@@ -0,0 +1,125 @@
+(* See MLI for comments on how this works and the complexity requirements *)
+
+type size_kind =
+  [ `constant
+    | `immutable
+    | `updatable
+    | `ephemeral
+  ]
+
+type tracker =
+  { mutable size: Sizeops.Size.t
+    ; mutable parent: parent
+    ; item_overhead: Sizeops.Size.t (* TODO: only for updatable! *)
+  }
+
+and parent = Immutable | UpdatableUnset | UpdatableParent of tracker
+
+type 'a t = tracker constraint 'a = [< size_kind]
+
+let unboxed = Sizeops.Size.of_int 0 (* no extra space taken up beyond that for the field itself *)
+let boxed = Sizeops.Size.of_words 1
+let make size = { size; parent = Immutable; item_overhead = unboxed }
+
+let record_field = Sizeops.Size.of_words 0
+
+let bool _ = make unboxed
+let char _ = make unboxed
+let int _ = make unboxed
+let unit () = make unboxed
+
+let float _ = make boxed
+let int32 _ = make boxed
+let int64 _ = make boxed
+let nativeint _ = make boxed
+
+let bytes b = b |> Bytes.length |> Sizeops.Size.of_bytes |> make
+let string s = s |> String.length |> Sizeops.Size.of_bytes |> make
+
+let set_parent t ~parent =
+  match t.parent with
+  | Immutable -> () (* no updates possible *)
+  | UpdatableUnset ->
+      t.parent <- UpdatableParent parent
+  | UpdatableParent _ ->
+      invalid_arg "expression already has a parent set"
+
+let unset_parent t =
+  match t.parent with
+  | Immutable -> () (* no updates possible *)
+  | UpdatableParent _ ->
+      t.parent <- UpdatableUnset
+  | UpdatableUnset ->
+      invalid_arg "expression's parent already removed"
+
+let add a b =
+  let size = Sizeops.Size.(a.size + b.size) in
+  match a.parent, b.parent with
+  | Immutable, Immutable -> make size
+  | _ ->
+      let t = { size; parent = UpdatableUnset; item_overhead = unboxed } in
+      set_parent a ~parent:t;
+      set_parent b ~parent:t;
+      t
+
+let option size_of = function
+  | None -> make boxed
+  | Some e -> add (make boxed) @@ size_of e
+
+let array size_of a =
+  make @@ match Sizeops.Size.to_int_opt (size_of a.(0)).size with
+  | None -> Sizeops.Size.invalid
+  | Some e ->
+      (* TODO: multiplication should be part of sizeops? *)
+      let n = Array.length a in
+      let res = n * e in
+      if res < n || res < e then Sizeops.Size.invalid
+      else Sizeops.Size.of_int res
+
+type ('a, 'b) fields = 'b t
+let record_start _t = { size = boxed; parent = UpdatableUnset; item_overhead = unboxed }
+
+let record_add_immutable field t =
+  set_parent field ~parent:t;
+  t.size <- Sizeops.Size.(t.size + record_field + field.size);
+  t
+
+let record_add_mutable field t : (_, [> `ephemeral]) fields =
+  (* no need to set up size updates, the results are ephemeral anyway *)
+  t.size <- Sizeops.Size.(t.size + record_field + field.size);
+  t
+
+let record_end t = t
+
+let container_create ~initial ~item_overhead =
+  let t = { size = initial.size; parent = UpdatableUnset; item_overhead = item_overhead.size } in
+  set_parent initial ~parent:t;
+  t
+
+let rec container_add t n =
+  t.size <- Sizeops.Size.(t.size + n);
+  match t.parent with
+  | UpdatableParent p -> container_add p n
+  | UpdatableUnset | Immutable -> ()
+
+let container_add_element t e =
+  set_parent e ~parent:t;
+  container_add t Sizeops.Size.(e.size + t.item_overhead)
+
+let rec container_sub t n =
+  t.size <- Sizeops.Size.(t.size - n);
+  match t.parent with
+  | UpdatableParent p -> container_sub p n
+  | UpdatableUnset | Immutable -> ()
+
+let container_remove_element t e =
+  unset_parent e;
+  container_sub t Sizeops.Size.(e.size + t.item_overhead)
+
+let size_of t = t.size
+
+let pp ppf t =
+  match Sizeops.Size.to_int_opt t.size with
+  | None -> Format.pp_print_string ppf "OVERFLOW"
+  | Some x -> Format.pp_print_int ppf x
+
---
+++ tools/ocaml/libs/xb/memory_size.mli
@@ -0,0 +1,200 @@
+(**
+  Assigns owners to long lived values in memory.
+  This can be used to introduce quotas per owner to reduce the likelihood that a single entity drives the
+  application out of memory.
+
+  We cannot guarantee that we won't get OOM killed (that depends on how much memory other processes
+  in the system use), but we can ensure that our memory usage is bounded if:
+    * all long lived values in memory are assigned an owner
+    * when an owner exceeds its quota it is not allowed to consume more memory
+    * set an OOM score adjustment to ensure we don't get killed just because we're the highest
+    memory user (done by startup script) as long as we don't use more than X% of RAM
+    * tweak the garbage collector to reduce max overhead
+
+  Memory used temporarily by packet processing is not measured, this is assumed to be "small"
+  compared to overall memory usage, and this can be tested by fuzzing to not exceed a certain upper
+  bound.
+  This module only takes care of the first part - tracking memory usage.
+
+  Memory usage needs to be tracked efficiently (so we don't consume a lot more memory just to track
+  it, and we don't degrade performance by walking all the values on each action).
+    * all operations in this module are O(1), except removal of mixed containers
+    * all memory used by long-lived values in this module are O(1)
+    * doesn't rely on the Obj module: memory tracking is opt-in for data structures
+
+  A given data structure can hold memory belonging to different owners (most obvious example: the
+  xenstore tree itself), hence ownership isn't always hierarchical, but attached to individual
+  values. However we avoid keeping an explicit list of values owned by each owner, and instead just
+  update a counter by adding and subtracting sizes on each operation.
+
+  To detect bugs there is also a "reference" implementation that is not O(1) that can be used
+  by fuzzers to check that the optimized implementation here is correct.
+*)
+
+(** A module to compute the size of a data structure incrementally in O(1) time.
+
+      Only types where the size can be computed in O(1) are implemented below.
+
+      Not implemented:
+        * exn (because it can contain different element types)
+        * 'a list - there is a separate SizedList module that can be used instead
+        * _ format6 - not typically stored in xenstored, size is implementation version dependent
+   *)
+
+type size_kind =
+  [ `constant
+  (** all values of this type have exactly the same size.
+      This means they can be part of arrays and the size of the array will stay constant
+      (even if the values change) *)
+
+  | `immutable
+  (** the size of this value doesn't change, but the size of different values of this type is not
+      the same. Cannot be part of primitive arrays since we'd no longer be able to compute its
+      size when elements are changed
+    *)
+
+  | `updatable
+  (** this refers to a value that has a mutable component, but we are tracking updates to the size
+      of the value, and this expression is updated too.
+      Can still be nested inside other mutable data structures *)
+
+  | `ephemeral
+  (** this size expression is only valid at the current moment in time.
+      It contains records with mutable fields where we cannot track updates to their size.
+      This cannot be nested inside other mutable data structures.
+      *)
+  ]
+
+type 'a t constraint 'a = [< size_kind]
+(** A size expression for data structures.
+
+    The type parameter declares how size updates propagate.
+
+  *)
+
+val bool: bool -> [>`constant] t
+(** [bool] computes the size of a bool.
+
+    O(1) complexity, it is a constant. *)
+
+val char: char -> [>`constant ] t
+(** [bool] computes the size of a bool.
+
+    O(1) complexity, it is a constant. *)
+
+val float: float -> [>`constant] t
+(** [float] computes the size of a float.
+
+    O(1) complexity, it is a constant. *)
+
+val int: int -> [>`constant ] t
+(** [int] computes the size of an int.
+
+    O(1) complexity, it is a constant. *)
+
+val int32: int32 -> [>`constant] t
+(** [int32] computes the size of an int32.
+
+    O(1) complexity, it is a constant. *)
+
+val int64: int64 -> [>`constant] t
+(** [int64] computes the size of an int32.
+
+    O(1) complexity, it is a constant. *)
+
+val nativeint: nativeint -> [>`constant] t
+(** [nativeint] computes the size of a nativeint.
+
+    O(1) complexity, it is a constant. *)
+
+val unit: unit -> [>`constant] t
+(** [unit] computes the size of a unit.
+
+    O(1) complexity, it is a constant. *)
+
+val bytes: bytes -> [>`immutable ] t
+(** [bytes] computes the size of a [bytes value]. This is mutable but its size is constant.
+
+    O(1) complexity since [bytes] store their size explicitly. *)
+
+val string: string -> [>`immutable] t
+(** [string] computes the size of a string.
+
+    O(1) complexity since strings store their size explicitly. *)
+
+val option: ('a -> 'a t) -> 'a option -> 'a t
+(** [option element_immutable_size_of x] computes the size of ['a option] using [element_immutable_size_of] to compute
+    the size of its element.
+
+    O(1) complexity if [element_immutable_size_of] is
+    *)
+
+val array: ('a -> [<`constant] t) -> 'a array -> [>`constant] t
+(** [array size_of_element arr] computes the size of the array [arr],
+    using [size_of_element].
+    The elements must have a constant size regardless of their value. *)
+
+type ('a, 'b) fields constraint 'b = [<size_kind]
+(** size expression for record fields.
+    'a is the type of the record
+    'b is the [size_kind]
+ *)
+
+val record_start: 'a -> ('a, 'b) fields
+(** [record_start r] begins computing the size of record [r].
+
+    O(1) complexity.
+ *)
+
+val record_add_immutable: 'b t -> ('a, 'b) fields -> ('a, 'b) fields
+(** [record_add_immutable field acc] adds the size of [field] to [acc].
+
+    The field cannot be mutable, because if it was then
+      we wouldn't be able to automatically track size of updates for it.
+    However the type checker cannot ensure this.
+
+    Note that this is unrelated to the *type* of the field:
+      an immutable field can still point to a mutable type, which is fine.
+
+    O(1) complexity.
+ *)
+
+val record_add_mutable: _ t -> ('a, ([>`ephemeral] as 'b)) fields -> ('a, 'b) fields
+(** [record_add_mutable field acc] adds the size of [field] to [acc].
+
+    The field is mutable.
+    However the type checker cannot ensure this.
+
+    O(1) complexity.
+ *)
+
+val record_end: ('a, 'b) fields -> 'b t
+(** [record_end acc] finishes computing the size of a record.
+
+  O(1) complexity.
+ *)
+
+val container_create: initial:[<`constant | `immutable | `updatable] t -> item_overhead:[<`constant|`immutable] t -> [>`updatable] t
+(** [container_create ~initial ~item_overhead] creates a new size update tracker, with [record]'s size as starting
+    point and [item_overhead] overhead per element.
+
+    O(1) complexity *)
+
+val container_add_element: [<`constant | `immutable | `updatable] t -> [<`updatable] t -> unit
+(** [container_add_element element container] adds [element]'s size to [container] and tracks
+    updates to it. The size expression of the container will always reflect the size of all items
+    elements. It includes the [item_overhad].
+
+    O(1) complexity. *)
+
+val container_remove_element: [<`constant | `immutable | `updatable] t -> [<`updatable] t -> unit
+(** [container_remove_element element container] removes [element]'s size from the [container].
+    It removes the [item_overhead] too.
+
+    O(1) complexity. *)
+
+val size_of: [>`constant|`immutable] t -> Sizeops.Size.t
+(** [size_of t] is the size of [t]. Can only be inspected directly when it is a constant *)
+
+val pp: Format.formatter -> _ t -> unit
+(** [pp formatter size] pretty prints the size *)
