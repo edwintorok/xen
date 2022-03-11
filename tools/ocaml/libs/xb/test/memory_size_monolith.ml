536f33fadddec78955639d2751b70426ec183c3d
Edwin Török <edvin.torok@citrix.com>
2022-03-11 15:51:40 +0000

    monolith tests for buffer size

--- tools/ocaml/libs/xb/test/memory_size_monolith.ml
+++ tools/ocaml/libs/xb/test/memory_size_monolith.ml
@@ -33,6 +33,25 @@ let declare_size_of name typ refs candidate =
   let size x = Option.value ~default:max_int @@ Sizeops.Size.to_int_opt @@ refs x in
   declare (name ^ ".size_of") (typ ^> int) size (wrap_size_of candidate)
 
+let zero _ = 0
+
+let size_t = deconstructible @@ fun s -> s |> Sizeops.Size.to_int_opt |> Print.option Print.int
+
+let declare_size_of_reachable name typ candidate =
+  let check_size t actual =
+    let expected = Sizeops.Size.of_words @@ Obj.reachable_words (Obj.repr t) in
+    let expected = Option.value ~default:max_int @@ Sizeops.Size.to_int_opt expected in
+    let expected = expected * Sys.word_size / 8 in
+    if expected <= actual then
+      Valid actual
+    else
+      Invalid (fun d ->
+        let open PPrint in
+        Print.assert_ (d ^^ Print.string " <= " ^^ Print.int actual) ^^
+        Print.candidate_finds d)
+  in
+  declare (name ^ ".size_of") (typ ^?> int) check_size (wrap_size_of candidate)
+
 let () =
   let buffer = declare_abstract_type ~var:"buffer" () in
   declare "Buffer.create" (small_int ^> buffer) R.Buffer.create C.Buffer.create;
@@ -41,7 +60,7 @@ let () =
   declare "Buffer.add_substring" (buffer ^> buf_string ^> small_int ^> small_int ^!> unit) R.Buffer.add_substring C.Buffer.add_substring;
   declare "Buffer.reset" (buffer ^> unit) R.Buffer.reset C.Buffer.reset;
   declare "Buffer.contents" (buffer ^> str) R.Buffer.contents C.Buffer.contents;
-  declare_size_of "Buffer" buffer R.Buffer.size_of C.Buffer.size_of
+  declare_size_of_reachable "Buffer" buffer C.Buffer.size_of
 
 let () =
   let fuel = 100 in
