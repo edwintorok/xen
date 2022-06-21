5d1ac633b7e8708046c48a8c95074c4565970074
Edwin Török <edvin.torok@citrix.com>
2022-06-21 14:27:45 +0100

    WiP

--- tools/ocaml/quotaverify.mli
+++ tools/ocaml/quotaverify.mli
@@ -41,8 +41,6 @@
 
   * [Quota_i] is a user supplied configuration value, set to achieve the desired [AvailableMemory]
   * [Constant_i] is a constant factor to ensure linear memory usage.
-  Logarithmic memory usage (e.g. for tree depth) can be accounted for by introducing a quota on the number of items,
-  and then adding the maximum value of that as a constant here.
   The application is of course allowed to optimize memory usage (e.g. via sharing) and use less memory
   than computed here, but that is already allowed by the initial equation.
   A small [Constant_i] is desireable, however an upper bound on this is eventually ensured by
@@ -54,5 +52,89 @@
   algorithmic and space complexity, except we're not interested only in what happens
   when the input goes to infinity, we're also interested whether there are any
   large constant factors.
+*)
 
+open Xenbus
+
+module Approximation : sig
+  type 'a t = 'a -> Size_tracker.t -> Size_tracker.t * Size_tracker.t
+  (** a memory usage approximation model [size_of_x -> (lb, ub)]
+   Given a value [x] and an approximate size [size_of_x] compute bounds [lb, ub],
+   such that [lb <= real_memory_usage(x) <= ub]
+   *)
+
+  val exact: 'a t
+  (** [exact] is an exact model, [lb = ub = size_of_x].
+    Used for primitive types.
+  *)
+
+  val linear: ?offset: Size_tracker.t -> ?multiplier:int -> Size_tracker.t seq -> 'a t
+  (** [linear ?offset ?multiplier examples]
+    [lb = offset], [ub = offset + multiplier * size_of_x].
+    if [multiplier] is provided then it verifies that the formula works on the supplied examples,
+    if any.
+    if [multiplier] is not provided then [examples] must be, and [offset] and [multiplier] are computed as needed.
+  *)
+
+  val bounded_log: max_count:int -> 'a t -> 'a t
+  (** [bounded_log ~max_count approx] is the approximation approx with its constant factors
+    modified such that [log2(max_count)] is included.
+  *)
+end
+
+
+type quota_error =
+ { calculated: Size_tracker.t
+ ; measured: Size_tracker.t
+ ; msg: string
+ }
+
+val check: Approximation.t -> ('a -> Size_tracker.t) -> 'a -> (Size_tracker.t, quota_error) result
+(** [check approximation size_of x] checks whether the computed [size_of x] matches the bounds computed 
+     by the [approximation]
+*)
+
+type 'a t = 'a -> (Size_tracker.t, quota_error) result (** verify quota when supplied input of type ['a] *)
+
+module Exact : sig
+  val unboxed: 'a t
+  (** [unboxed] is the size of unboxed OCaml types, i.e. just 1 word, no GC overhead *)
+
+  val words: int -> 'a t
+  (** [words size] asserts that [memory_size(x) = size+1] in words,
+  where +1 accounts for the extra GC header before each value *)
+
+  val string_like: int -> 'a t
+  (** [string_like n] is the memory used by a [string] or [bytes] with [n] characters. *)
+end
+
+val pair : 'a t -> 'b t -> ('a * 'b) t
+(** [pair a b] verifies the memory used by the pair of [a, b] *)
+
+val linear : ?baseline:Size_tracker.t -> ?multiplier:int -> ('a seq -> 'b) -> 'a t -> (int -> 'b) t
+(** [linear ?baseline ?multiplier gen element] asserts that the memory used by [N] elements follows a linear model:
+  Confusion here between memory usage model and N* sequence.... lets separate
+  linear bound!
+
+
+  If [multiplier] is specified then this is required to be an exact match:
+    [memory_used = baseline + N * multiplier * element]
+  Otherwise it is allowed to be approximate and a suitable [baseline] and [multiplier] is determined automatically at startup.
+    [memory_used(gen(n)) ≤ baseline + n * multiplier * element]
+*)
+
+val tree_like: max_count:int -> (int -> 'a) t -> (int -> 'a) t
+(** [tree_like ~max_count elements] verifies that [elements] follows an [n*log2(n)] memory usage model,
+    where [n ≤ max_count], a constant upper bound.
+    This will in turn [log(n)] into a constant factor.
+    (We could've also turned the entire [n*log(n)] into a constant factor, but may have been too large to be useful
+    when we want more accurate memory usage models)
+
+    [elements] can be specified using [linear].
+*)
+
+val bounded_by: bytes:int -> 'a t -> 'a t
+(** [bounded_by ~bytes] verifies that the memory used by ['a] is bounded by [bytes].
+  Typically this implies that some quota mechanism is used to limit the construction of ['a],
+  so it doesn't become too big.
 *)
