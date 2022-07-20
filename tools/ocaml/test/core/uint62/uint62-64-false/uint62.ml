a32664fcfa60acf186bb995df2c1f3d2de01f410
Edwin Török <edvin.torok@citrix.com>
2022-07-20 15:48:12 +0100

    WiP

--- tools/ocaml/test/core/uint62/uint62-64-false/uint62.ml
+++
@@ -1,5 +0,0 @@
-type t = int64
-
-let of_int64 i = Int64.logand i 0x3FFF_FFFF_FFFF_FFFFL
-
-let to_int64 (i : t) = i
--- tools/ocaml/test/core/uint62/uint62-64-true/uint62.ml
+++
@@ -1,5 +0,0 @@
-type t = int
-
-let of_int64 i = Int64.to_int i land max_int [@@ocaml.inline]
-
-let to_int64 = Int64.of_int
