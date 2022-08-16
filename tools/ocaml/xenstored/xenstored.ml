95cc4694d09a551fcdd0a886d4a939292e50b2fb
Edwin Török <edvin.torok@citrix.com>
2022-08-16 10:17:14 +0100

    tools/ocaml/xenstored: drop direct systemd dependency
    launch-xenstore already calls systemd-notify, no need to call it twice.
    Also simplifies the build system by removing one conditional link flag.
    
    Signed-off-by: Edwin Török <edvin.torok@citrix.com>

--- tools/ocaml/xenstored/xenstored.ml
+++ tools/ocaml/xenstored/xenstored.ml
@@ -614,7 +614,6 @@ let () =
     process_domains store cons domains
   in
 
-  Systemd.sd_notify_ready ();
   let live_update = ref false in
   while not (!quit && Connections.prevents_quit cons = [])
   do
