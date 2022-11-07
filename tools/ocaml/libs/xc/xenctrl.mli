f6e504a5adcb122158fa2bebd668d2bfe87cf4da
Ian Campbell <ian.campbell@citrix.com>
2022-11-07 10:41:40 +0000

    tools/ocaml/libs/xc: add interface to detect PV drivers in HVM guests
    Required by xapi.
    
    Taken from xen-api-libs.hg/xc and adjusted for upstream libxc+ocaml.
    
    Signed-off-by: Ian Campbell <ian.campbell@citrix.com>
    Signed-off-by: Edwin Török <edvin.torok@citrix.com>
    ---
    This was part of the XenServer patchqueue for >9 years, it is a binding
    to an existing xenctrl.h API for a specific purpose.
    It got rewritten to use the newly introduced hvm_param_get binding.
    Code could/should be moved to xenopsd perhaps.
    Signed-off-by: Edwin Török <edvin.torok@citrix.com>

--- tools/ocaml/libs/xc/xenctrl.mli
+++ tools/ocaml/libs/xc/xenctrl.mli
@@ -290,6 +290,7 @@ external domain_deassign_device: handle -> domid -> (int * int * int * int) -> u
 external domain_test_assign_device: handle -> domid -> (int * int * int * int) -> bool
   = "stub_xc_domain_test_assign_device"
 
+val hvm_check_pvdriver : handle -> domid -> bool
 external version : handle -> version = "stub_xc_version_version"
 external version_compile_info : handle -> compile_info
   = "stub_xc_version_compile_info"
