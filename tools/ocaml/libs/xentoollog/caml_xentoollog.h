68da6467d0306a48455ef8de9139c75b4718710c
Edwin Török <edvin.torok@citrix.com>
2022-12-05 13:51:06 +0000

    tools/ocaml/libs/xentoollog: avoid use-after-free in finalizer
    The finalizer is needed to avoid memory leaks if we create a logger and raise
    an exception before passing it to another Xen library that would use it.
    
    However we have no control over the lifetime of the logger once passed
    into the Xen library, it'll use it as long as its handle is open,
    which might be longer than this value is alive on the OCaml side.
    
    To avoid use after free introduce a reference counting scheme:
     * before passing the logger to a Xen C library we need to use 'acquire_ref'
     * after the C library is done with the logger call 'drop_ref'
     (e.g. if allocating handle failed, or in the finalizer after closing the handle)
     * this C library starts out with a reference count of 1 and the finalizer sets
       it to 0
     * memory is only freed on the C side when reference counter reaches 0
    
    This can be seen by running some of the tests (e.g. test/list_domains)
    with OCAMLRUNPARAM=c which calls all finalizers on exit: it currently
    segfaults.
    
    Note that Xen C libraries do not call xtl_logger_destroy on the passed in
    logger, but only on loggers it allocates itself.
    
    There are no crashes and no memory leaks now:
    ```
    OCAMLRUNPARAM=c LD_LIBRARY_PATH=../libs/light:../libs/ctrl:../libs/guest valgrind --leak-check=full test/list_domains
    ==3520620== Memcheck, a memory error detector
    ==3520620== Copyright (C) 2002-2017, and GNU GPL'd, by Julian Seward et al.
    ==3520620== Using Valgrind-3.18.1 and LibVEX; rerun with -h for copyright info
    ==3520620== Command: test/list_domains
    ==3520620==
    Info: test: stub_xtl_test -- test 1
    Info: test errno: errno=38: stub_xtl_test -- test 2
    Critical: critical: stub_xtl_test -- critical
    Progress testing 100% (100/100)
    Error: xencall: errno=13: Could not obtain handle on privileged command interface
    Error: libxl: errno=13: libxl.c:103:libxl_ctx_alloc: cannot open libxc handle
    ==3520620== Syscall param sigaltstack(ss) points to uninitialised byte(s)
    ==3520620==    at 0x4A92D4B: sigaltstack (syscall-template.S:120)
    ==3520620==    by 0x181B55: caml_stop_stack_overflow_detection (signals_nat.c:350)
    ==3520620==    by 0x181B55: caml_terminate_signals (signals_nat.c:317)
    ==3520620==    by 0x1A0573: caml_startup_common (startup_nat.c:161)
    ==3520620==    by 0x1A05EE: caml_startup_exn (startup_nat.c:167)
    ==3520620==    by 0x1A05EE: caml_startup (startup_nat.c:172)
    ==3520620==    by 0x1A05EE: caml_main (startup_nat.c:179)
    ==3520620==    by 0x130311: main (main.c:37)
    ==3520620==  Address 0x1ffefffc70 is on thread 1's stack
    ==3520620==  in frame #1, created by caml_terminate_signals (signals_nat.c:306)
    ==3520620==
    Fatal error: exception Xenlight.Error(_)
    ==3520620==
    ==3520620== HEAP SUMMARY:
    ==3520620==     in use at exit: 3,712,960 bytes in 70 blocks
    ==3520620==   total heap usage: 128 allocs, 58 frees, 4,811,077 bytes allocated
    ==3520620==
    ==3520620== LEAK SUMMARY:
    ==3520620==    definitely lost: 0 bytes in 0 blocks
    ==3520620==    indirectly lost: 0 bytes in 0 blocks
    ==3520620==      possibly lost: 0 bytes in 0 blocks
    ```
    
    It is also not safe to call any OCaml callbacks while inside an OCaml finalizer
    according to the manual, so introduce an 'stub_xtl_disable' function to disable
    logging that must be called in a finalizer.
    
    Signed-off-by: Edwin Török <edvin.torok@citrix.com>

--- tools/ocaml/libs/xentoollog/caml_xentoollog.h
+++ tools/ocaml/libs/xentoollog/caml_xentoollog.h
@@ -18,7 +18,65 @@ struct caml_xtl {
     xentoollog_logger vtable;
     char *vmessage_cb;
     char *progress_cb;
+    int refs;
+    int disabled;
 };
 
-#define Xtl_val(x)(*((struct caml_xtl **) Data_custom_val(x)))
+/* [stub_xtl_acquire_ref(xtl)] must be called before passing the logger to a
+   Xen library: the logger needs to stay alive for as long as the C library
+   might use it, and there are no calls from the C library to tell when it is
+   stopped using it */
+struct caml_xtl* stub_xtl_acquire_ref(value);
 
+/* [stub_xtl-disable(xtl)] disables invoking any OCaml callbacks from this
+   logger.
+   This MUST be called in OCaml finalizers of other Xen C libraries, BEFORE
+   closing the handle of those libraries (which may or may not invoke the
+   logger).
+   It is not safe to call other OCaml functions while inside a finalizer.
+*/
+void stub_xtl_disable(struct caml_xtl*);
+
+/* [stub_xtl_drop_ref(&xtl)] call this after you have closed the underlying Xen
+    C library that used the logger.
+    The pointer will be set to NULL to detect double free attempts.
+*/
+void stub_xtl_drop_ref(struct caml_xtl**);
+
+/*
+  Recommended usage (note how we store 2 fields in the custom val, instead of
+  just the usual pointer):
+
+  typedef struct {
+    yourxenlib_ctx* ctx;
+    struct caml_xtl* logger;
+  } ctx_val_t;
+
+  #define Ctx_val(x) ((ctx_val_t *) Data_custom_val(x))
+
+  static void yourstub_finalize(value)
+  {
+    stub_xtl_disable(Ctx_val(ctx)->logger);
+    yourxenlib_free(Ctx_val(ctx)->ctx);
+    stub_xtl_drop_ref(&Ctx_val(ctx)->logger);
+  }
+
+  CAMLprim value yourstub_alloc(value logger)
+  {
+    CAMLparam1(logger);
+    CAMLlocal1(handle);
+    ctx_val_t ctxval;
+
+    ctxval.logger = stub_xtl_acquire_ref(logger);
+    ret = yourxenlib_alloc(&ctxval.logger);
+    if (ret) {
+        stub_xtl_drop_ref(&ctxval.logger);
+        failwith_yourlib(...);
+    }
+
+    handle = caml_alloc_custom(&yourlib_custom_ops, sizeof(ctxval), 0, 1);
+    *Ctx_val(handle) = ctxval;
+
+    CAMLreturn(handle);
+  }
+*/
