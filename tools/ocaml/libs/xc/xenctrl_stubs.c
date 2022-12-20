edwintorok/xen

commit 8b3c06a3e545204515e50733669ad6f5c7bddfd8

tools/ocaml/xenctrl: OCaml 5 support, fix use-after-free
The binding for xc_interface_close() free the underlying handle while leaving
the Ocaml object still in scope and usable.  This would make it easy to suffer
a use-after-free, if it weren't for the fact that the typical usage is as a
singleton that lives for the lifetime of the program.

Ocaml 5 no longer permits storing a naked C pointer in an Ocaml value.

Therefore, use a Custom block.  This allows us to use the finaliser callback
to call xc_interface_close(), if the Ocaml object goes out of scope.

Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
Acked-by: Christian Lindig <christian.lindig@citrix.com>

tools/ocaml/libs/xc/xenctrl_stubs.c
@@ -37,12 +37,27 @@
 
 #include "mmap_stubs.h"
 
-#define _H(__h) ((xc_interface *)(__h))
+#define _H(__h) (*((xc_interface **)Data_custom_val(__h)))
 #define _D(__d) ((uint32_t)Int_val(__d))
 
 #ifndef Val_none
 #define Val_none (Val_int(0))
 #endif
+
+static void stub_xenctrl_finalize(value v)
+{
+	xc_interface_close(_H(v));
+}
+
+static struct custom_operations xenctrl_ops = {
+	.identifier  = "xenctrl",
+	.finalize    = stub_xenctrl_finalize,
+	.compare     = custom_compare_default,     /* Can't compare     */
+	.hash        = custom_hash_default,        /* Can't hash        */
+	.serialize   = custom_serialize_default,   /* Can't serialize   */
+	.deserialize = custom_deserialize_default, /* Can't deserialize */
+	.compare_ext = custom_compare_ext_default, /* Can't compare     */
+};
 
 #define string_of_option_array(array, index) \
 	((Field(array, index) == Val_none) ? NULL : String_val(Field(Field(array, index), 0)))
@@ -70,26 +85,20 @@ static void Noreturn failwith_xc(xc_interface *xch)
 CAMLprim value stub_xc_interface_open(void)
 {
 	CAMLparam0();
-        xc_interface *xch;
-
-	/* Don't assert XC_OPENFLAG_NON_REENTRANT because these bindings
-	 * do not prevent re-entrancy to libxc */
-        xch = xc_interface_open(NULL, NULL, 0);
-        if (xch == NULL)
-		failwith_xc(NULL);
-        CAMLreturn((value)xch);
-}
-
-
-CAMLprim value stub_xc_interface_close(value xch)
-{
-	CAMLparam1(xch);
+	CAMLlocal1(result);
+	xc_interface *xch;
 
 	caml_enter_blocking_section();
-	xc_interface_close(_H(xch));
+	xch = xc_interface_open(NULL, NULL, 0);
 	caml_leave_blocking_section();
 
-	CAMLreturn(Val_unit);
+	if ( !xch )
+		failwith_xc(xch);
+
+	result = caml_alloc_custom(&xenctrl_ops, sizeof(xch), 0, 1);
+	_H(result) = xch;
+
+	CAMLreturn(result);
 }
 
 static void domain_handle_of_uuid_string(xen_domain_handle_t h,

tools/ocaml/libs/xc/xenctrl.ml
@@ -175,7 +175,6 @@ exception Error of string
 type handle
 
 external interface_open: unit -> handle = "stub_xc_interface_open"
-external interface_close: handle -> unit = "stub_xc_interface_close"
 
 let handle = ref None
 
@@ -183,7 +182,7 @@ let get_handle () = !handle
 
 let close_handle () =
 	match !handle with
-	| Some h -> handle := None; interface_close h
+	| Some h -> handle := None
 	| None -> ()
 
 let with_intf f =

tools/ocaml/libs/xc/xenctrl.mli
@@ -146,7 +146,6 @@ type shutdown_reason = Poweroff | Reboot | Suspend | Crash | Watchdog | Soft_res
 exception Error of string
 type handle
 external interface_open : unit -> handle = "stub_xc_interface_open"
-external interface_close : handle -> unit = "stub_xc_interface_close"
 
 (** [with_intf f] runs [f] with a global handle that is opened on demand
  * and kept open. Conceptually, a client should use either

commit 620e703ea614e2db643f07744354e37ae6be36ab

fix
Fixes: 8b3c06a3e5 ("tools/ocaml/xenctrl: OCaml 5 support, fix use-after-free")

tools/ocaml/libs/xc/xenctrl_stubs.c
@@ -985,10 +985,11 @@ CAMLprim value stub_xc_version_compile_info(value xch)
 static value xc_version_single_string(value xch, int code, void *info)
 {
     CAMLparam1(xch);
+    xc_interface *xc = _H(xch);
     int retval;
 
     caml_enter_blocking_section();
-    retval = xc_version(_H(xch), code, info);
+    retval = xc_version(xc, code, info);
     caml_leave_blocking_section();
 
     if (retval)
@@ -1027,6 +1028,7 @@ CAMLprim value stub_map_foreign_range(value xch, value dom,
 {
     CAMLparam4(xch, dom, size, mfn);
     CAMLlocal1(result);
+    xc_interface *xc = _H(xch);
     struct mmap_interface *intf;
     uint32_t c_dom;
     unsigned long c_mfn;
@@ -1039,7 +1041,7 @@ CAMLprim value stub_map_foreign_range(value xch, value dom,
     c_dom = _D(dom);
     c_mfn = Nativeint_val(mfn);
     caml_enter_blocking_section();
-    intf->addr = xc_map_foreign_range(_H(xch), c_dom,
+    intf->addr = xc_map_foreign_range(xc, c_dom,
                                       intf->len, PROT_READ|PROT_WRITE,
                                       c_mfn);
     caml_leave_blocking_section();
@@ -1053,10 +1055,11 @@ CAMLprim value stub_sched_credit_domain_get(value xch, value domid)
     CAMLparam2(xch, domid);
     CAMLlocal1(sdom);
     struct xen_domctl_sched_credit c_sdom;
+    xc_interface *xc = _H(xch);
     int ret;
 
     caml_enter_blocking_section();
-    ret = xc_sched_credit_domain_get(_H(xch), _D(domid), &c_sdom);
+    ret = xc_sched_credit_domain_get(xc, _D(domid), &c_sdom);
     caml_leave_blocking_section();
     if (ret != 0)
         failwith_xc(_H(xch));
@@ -1073,12 +1076,13 @@ CAMLprim value stub_sched_credit_domain_set(value xch, value domid,
 {
     CAMLparam3(xch, domid, sdom);
     struct xen_domctl_sched_credit c_sdom;
+    xc_interface *xc = _H(xch);
     int ret;
 
     c_sdom.weight = Int_val(Field(sdom, 0));
     c_sdom.cap = Int_val(Field(sdom, 1));
     caml_enter_blocking_section();
-    ret = xc_sched_credit_domain_set(_H(xch), _D(domid), &c_sdom);
+    ret = xc_sched_credit_domain_set(xc, _D(domid), &c_sdom);
     caml_leave_blocking_section();
     if (ret != 0)
         failwith_xc(_H(xch));
@@ -1090,11 +1094,12 @@ CAMLprim value stub_shadow_allocation_get(value xch, value domid)
 {
     CAMLparam2(xch, domid);
     CAMLlocal1(mb);
+    xc_interface *xc = _H(xch);
     unsigned int c_mb;
     int ret;
 
     caml_enter_blocking_section();
-    ret = xc_shadow_control(_H(xch), _D(domid),
+    ret = xc_shadow_control(xc, _D(domid),
                 XEN_DOMCTL_SHADOW_OP_GET_ALLOCATION,
                 &c_mb, 0);
     caml_leave_blocking_section();
@@ -1109,12 +1114,13 @@ CAMLprim value stub_shadow_allocation_set(value xch, value domid,
                       value mb)
 {
     CAMLparam3(xch, domid, mb);
+    xc_interface *xc = _H(xch);
     unsigned int c_mb;
     int ret;
 
     c_mb = Int_val(mb);
     caml_enter_blocking_section();
-    ret = xc_shadow_control(_H(xch), _D(domid),
+    ret = xc_shadow_control(xc, _D(domid),
                 XEN_DOMCTL_SHADOW_OP_SET_ALLOCATION,
                 &c_mb, 0);
     caml_leave_blocking_section();
@@ -1188,11 +1194,12 @@ CAMLprim value stub_xc_domain_irq_permission(value xch, value domid,
 CAMLprim value stub_xc_hvm_param_get(value xch, value domid, value param)
 {
     CAMLparam3(xch, domid, param);
+    xc_interface *xc = _H(xch);
     uint64_t val;
     int ret;
 
     caml_enter_blocking_section();
-    ret = xc_hvm_param_get(_H(xch), _D(domid), Int_val(param), &val);
+    ret = xc_hvm_param_get(xc, _D(domid), Int_val(param), &val);
     caml_leave_blocking_section();
 
     if ( ret )
@@ -1204,10 +1211,11 @@ CAMLprim value stub_xc_hvm_param_get(value xch, value domid, value param)
 CAMLprim value stub_xc_hvm_param_set(value xch, value domid, value param, value val)
 {
     CAMLparam4(xch, domid, param, val);
+    xc_interface *xc = _H(xch);
     int ret;
 
     caml_enter_blocking_section();
-    ret = xc_hvm_param_set(_H(xch), _D(domid), Int_val(param), Int64_val(val));
+    ret = xc_hvm_param_set(xc, _D(domid), Int_val(param), Int64_val(val));
     caml_leave_blocking_section();
 
     if ( ret )