edwintorok/xen

commit 22d5affdf0cecfa6faae46fbaec68b8018835220

tools/ocaml/evtchn: OCaml 5 support, fix potential resource leak
There is no binding for xenevtchn_close().  In principle, this is a resource
leak, but the typical usage is as a singleton that lives for the lifetime of
the program.

Ocaml 5 no longer permits storing a naked C pointer in an Ocaml value.

Therefore, use a Custom block.  This allows us to use the finaliser callback
to call xenevtchn_close(), if the Ocaml object goes out of scope.

Signed-off-by: Edwin Török <edvin.torok@citrix.com>
Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
Acked-by: Christian Lindig <christian.lindig@citrix.com>

tools/ocaml/libs/eventchn/xeneventchn_stubs.c
@@ -33,7 +33,22 @@
 #include <caml/fail.h>
 #include <caml/signals.h>
 
-#define _H(__h) ((xenevtchn_handle *)(__h))
+#define _H(__h) (*((xenevtchn_handle **)Data_custom_val(__h)))
+
+static void stub_evtchn_finalize(value v)
+{
+	xenevtchn_close(_H(v));
+}
+
+static struct custom_operations xenevtchn_ops = {
+	.identifier  = "xenevtchn",
+	.finalize    = stub_evtchn_finalize,
+	.compare     = custom_compare_default,     /* Can't compare     */
+	.hash        = custom_hash_default,        /* Can't hash        */
+	.serialize   = custom_serialize_default,   /* Can't serialize   */
+	.deserialize = custom_deserialize_default, /* Can't deserialize */
+	.compare_ext = custom_compare_ext_default, /* Can't compare     */
+};
 
 CAMLprim value stub_eventchn_init(void)
 {
@@ -48,7 +63,9 @@ CAMLprim value stub_eventchn_init(void)
 	if (xce == NULL)
 		caml_failwith("open failed");
 
-	result = (value)xce;
+	result = caml_alloc_custom(&xenevtchn_ops, sizeof(xce), 0, 1);
+	_H(result) = xce;
+
 	CAMLreturn(result);
 }
 

commit 8d30139588ff4e9afb6b595ae7f614512b2723f2

tools/ocaml/libs/evtchn: fix race condition
See the manual https://v2.ocaml.org/manual/intfc.html#ss:parallel-execution-long-running-c-code
"After caml_release_runtime_system() was called and until
caml_acquire_runtime_system() is called, the C code must not access any OCaml
data, nor call any function of the run-time system, nor call back into OCaml
code."

_H was previously just a cast, and although it didn't comply with the 'no naked
pointers rule', it did comply with the above rule.
(no naked pointers is only active by default in OCaml 5+)

The commit that made to code compatible with 'no naked pointers mode' has thus
introduced a read race condition: the OCaml GC may have moved the value in
another thread, where the code in the current thread might've already stored
the value in a register, and thus it'd derefence a pointer to something
potentially entirely different (or crash if memory has been compacted and
deallocated).
That race condition is not visible by reviewing the diff of the commit though.

To find more remaining race conditions like this and to avoid reintroducing
this bug in the future I started developing a static analyzer to look for this
problem.

It has found the following problems in xeneventchn_stubs.c:
```
[Error][Race] DomainLock: must be held when dereferencing OCaml value xce (tools/ocaml/libs/eventchn/xeneventchn_stubs.c:133:2-133:151)
[Error][Race] DomainLock: must be held when dereferencing OCaml value xce (tools/ocaml/libs/eventchn/xeneventchn_stubs.c:150:2-150:118)
[Error][Race] DomainLock: must be held when dereferencing OCaml value xce (tools/ocaml/libs/eventchn/xeneventchn_stubs.c:116:2-116:110)
[Error][Race] DomainLock: must be held when dereferencing OCaml value xce (tools/ocaml/libs/eventchn/xeneventchn_stubs.c:182:2-182:90)
[Error][Race] DomainLock: must be held when dereferencing OCaml value xce (tools/ocaml/libs/eventchn/xeneventchn_stubs.c:166:2-166:110)
[Error][Race] DomainLock: must be held when dereferencing OCaml value xce (tools/ocaml/libs/eventchn/xeneventchn_stubs.c:201:2-201:93)
```

Fixes: 22d5affdf0 ("tools/ocaml/evtchn: OCaml 5 support, fix potential resource leak")

tools/ocaml/libs/eventchn/xeneventchn_stubs.c
@@ -110,10 +110,11 @@ CAMLprim value stub_eventchn_fd(value xce)
 CAMLprim value stub_eventchn_notify(value xce, value port)
 {
     CAMLparam2(xce, port);
+    xenevtchn_handle *xceh = _H(xce);
     int rc;
 
     caml_enter_blocking_section();
-    rc = xenevtchn_notify(_H(xce), Int_val(port));
+    rc = xenevtchn_notify(xceh, Int_val(port));
     caml_leave_blocking_section();
 
     if (rc == -1)
@@ -127,10 +128,11 @@ CAMLprim value stub_eventchn_bind_interdomain(value xce, value domid,
 {
     CAMLparam3(xce, domid, remote_port);
     CAMLlocal1(port);
+    xenevtchn_handle *xceh = _H(xce);
     xenevtchn_port_or_error_t rc;
 
     caml_enter_blocking_section();
-    rc = xenevtchn_bind_interdomain(_H(xce), Int_val(domid), Int_val(remote_port));
+    rc = xenevtchn_bind_interdomain(xceh, Int_val(domid), Int_val(remote_port));
     caml_leave_blocking_section();
 
     if (rc == -1)
@@ -144,10 +146,11 @@ CAMLprim value stub_eventchn_bind_virq(value xce, value virq_type)
 {
     CAMLparam2(xce, virq_type);
     CAMLlocal1(port);
+    xenevtchn_handle *xceh = _H(xce);
     xenevtchn_port_or_error_t rc;
 
     caml_enter_blocking_section();
-    rc = xenevtchn_bind_virq(_H(xce), Int_val(virq_type));
+    rc = xenevtchn_bind_virq(xceh, Int_val(virq_type));
     caml_leave_blocking_section();
 
     if (rc == -1)
@@ -160,10 +163,11 @@ CAMLprim value stub_eventchn_bind_virq(value xce, value virq_type)
 CAMLprim value stub_eventchn_unbind(value xce, value port)
 {
     CAMLparam2(xce, port);
+    xenevtchn_handle *xceh = _H(xce);
     int rc;
 
     caml_enter_blocking_section();
-    rc = xenevtchn_unbind(_H(xce), Int_val(port));
+    rc = xenevtchn_unbind(xceh, Int_val(port));
     caml_leave_blocking_section();
 
     if (rc == -1)
@@ -177,9 +181,10 @@ CAMLprim value stub_eventchn_pending(value xce)
     CAMLparam1(xce);
     CAMLlocal1(result);
     xenevtchn_port_or_error_t port;
+    xenevtchn_handle *xceh = _H(xce);
 
     caml_enter_blocking_section();
-    port = xenevtchn_pending(_H(xce));
+    port = xenevtchn_pending(xceh);
     caml_leave_blocking_section();
 
     if (port == -1)
@@ -193,12 +198,13 @@ CAMLprim value stub_eventchn_unmask(value xce, value _port)
 {
     CAMLparam2(xce, _port);
     evtchn_port_t port;
+    xenevtchn_handle *xceh = _H(xce);
     int rc;
 
     port = Int_val(_port);
 
     caml_enter_blocking_section();
-    rc = xenevtchn_unmask(_H(xce), port);
+    rc = xenevtchn_unmask(xceh, port);
     caml_leave_blocking_section();
 
     if (rc)